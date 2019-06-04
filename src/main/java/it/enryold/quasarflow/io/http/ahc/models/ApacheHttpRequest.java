2b407e9a3a427ee3b94f91da922fb29a41f28f1f
Enrico Vecchio <enrico.vecchio1@gmail.com>
2019-06-04 13:43:13 +0200

    Add AHC

---
+++ src/main/java/it/enryold/quasarflow/io/http/ahc/models/ApacheHttpRequest.java
@@ -0,0 +1,48 @@
+package it.enryold.quasarflow.io.http.ahc.models;
+
+import it.enryold.quasarflow.io.http.consts.QHttpConsts;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import java.util.UUID;
+
+public class ApacheHttpRequest<T> {
+
+    private T attachedDatas;
+    private String requestId;
+    private HttpUriRequest request;
+
+    public ApacheHttpRequest(HttpUriRequest request){
+        this.request = request;
+        this.requestId = UUID.randomUUID().toString();
+        addRequestIdHeader();
+    }
+
+    public ApacheHttpRequest(String requestId, HttpUriRequest request){
+        this.request = request;
+        this.requestId = requestId;
+        addRequestIdHeader();
+    }
+
+    public ApacheHttpRequest(String requestId, HttpUriRequest request, T attachedDatas){
+        this.request = request;
+        this.requestId = requestId;
+        this.attachedDatas = attachedDatas;
+        addRequestIdHeader();
+    }
+
+    private void addRequestIdHeader(){
+        this.request.addHeader(QHttpConsts.REQUEST_HEADER, requestId);
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public HttpUriRequest getRequest() {
+        return request;
+    }
+
+    public T getAttachedDatas() {
+        return attachedDatas;
+    }
+}
--- src/test/java/it/enryold/quasarflow/IOProcessorTests.java
+++ src/test/java/it/enryold/quasarflow/IOProcessorTests.java
@@ -7,12 +7,20 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import it.enryold.quasarflow.interfaces.IEmitterTask;
 import it.enryold.quasarflow.interfaces.IFlow;
+import it.enryold.quasarflow.io.http.ahc.ApacheHttpProcessor;
+import it.enryold.quasarflow.io.http.ahc.models.ApacheHttpRequest;
+import it.enryold.quasarflow.io.http.ahc.models.ApacheHttpResponse;
 import it.enryold.quasarflow.io.http.okhttp.OkHttpProcessor;
 import it.enryold.quasarflow.io.http.okhttp.models.OkHttpRequest;
 import it.enryold.quasarflow.io.http.okhttp.models.OkHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.BasicHttpEntity;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,18 +31,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IOProcessorTests extends TestUtils {
 
+  
 
     @AfterEach
     public void afterEach(){
 
         this.printRuntime();
-
-
     }
 
 
+
+
     @Test
-    public void testAsync() throws InterruptedException {
+    public void testOkHttpAsync() throws InterruptedException {
 
         String requestPrefix = "REQ";
         String dataPrefix = "DATA";
@@ -105,7 +114,7 @@ public class IOProcessorTests extends TestUtils {
 
 
     @Test
-    public void testSync() throws InterruptedException {
+    public void testOkHttpSync() throws InterruptedException {
 
         String requestPrefix = "REQ";
         String dataPrefix = "DATA";
@@ -178,7 +187,7 @@ public class IOProcessorTests extends TestUtils {
 
 
     @Test
-    public void testAsyncLoad() throws InterruptedException {
+    public void testOkHttpAsyncLoad() throws InterruptedException {
 
         String requestPrefix = "REQ";
         String dataPrefix = "DATA";
@@ -249,7 +258,7 @@ public class IOProcessorTests extends TestUtils {
 
 
     @Test
-    public void testSyncLoad() throws InterruptedException {
+    public void testOkHttpSyncLoad() throws InterruptedException {
 
         String requestPrefix = "REQ";
         String dataPrefix = "DATA";
@@ -317,4 +326,160 @@ public class IOProcessorTests extends TestUtils {
         //currentFlow.destroy();
 
     }
+
+
+    @Test
+    public void testAHC() throws InterruptedException {
+
+        String requestPrefix = "REQ";
+        String dataPrefix = "DATA";
+
+        int requests = 10;
+        int fibers = 4;
+        String url = "https://reqres.in/api/users/2";
+
+
+        IEmitterTask<ApacheHttpRequest<String>> requestTask = publisherChannel -> {
+            for(int i=0; i<requests; i++){
+
+                try {
+                    Map<String, String> payload = new HashMap<>();
+                    payload.put("name", "pippo"+i);
+                    payload.put("job", "pippo"+i);
+
+                    byte[] bytePayload = new ObjectMapper().writeValueAsBytes(payload);
+
+                    BasicHttpEntity httpEntity = new BasicHttpEntity();
+                    httpEntity.setContent(new ByteArrayInputStream(bytePayload));
+                    httpEntity.setContentLength(bytePayload.length);
+                    httpEntity.setContentType("application/json");
+
+
+                    HttpPut request = new HttpPut();
+                    request.setURI(URI.create(url));
+                    request.setEntity(httpEntity);
+
+                    ApacheHttpRequest<String> okHttpRequest = new ApacheHttpRequest<>(requestPrefix+i, request, dataPrefix+i);
+
+                    publisherChannel.send(okHttpRequest);
+
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        };
+
+        LinkedTransferQueue<ApacheHttpResponse<String>> responseQueue = resultQueue();
+
+        long startTime = System.currentTimeMillis();
+
+        IFlow currentFlow = QuasarFlow.newFlow()
+                .broadcastEmitter(requestTask)
+                .map(emitter -> new ApacheHttpProcessor<>(emitter)
+                        .processWithFanIn(fibers))
+                .addConsumer()
+                .consume(responseQueue::add)
+                .start();
+
+
+        List<ApacheHttpResponse<String>> results = this.getResults(responseQueue, requests, 3, TimeUnit.SECONDS);
+
+        System.out.println("Executed "+requests+" requests in parallel ("+fibers+" fibers) in: "+(System.currentTimeMillis()-startTime)+" ms");
+        System.out.println("Requests summed execution time: "+results.stream().mapToLong(ApacheHttpResponse::getExecution).sum()+" ms");
+
+        assertEquals(results.size(), requests);
+
+        for(ApacheHttpResponse<String> resp : results){
+            String idx0 = resp.getAttachedDatas().split(dataPrefix)[1];
+            String idx1 = resp.getRequestId().split(requestPrefix)[1];
+            assertEquals(idx0, idx1);
+        }
+
+        assertEquals(results.size(), requests);
+
+        //currentFlow.destroy();
+
+    }
+
+
+
+    @Test
+    public void testAHCLoad() throws InterruptedException {
+
+        String requestPrefix = "REQ";
+        String dataPrefix = "DATA";
+
+        int requests = 50;
+        int fibers = 50;
+        String url = "https://reqres.in/api/users/2";
+
+
+        IEmitterTask<ApacheHttpRequest<String>> requestTask = publisherChannel -> {
+            for(int i=0; i<requests; i++){
+
+                try {
+                    Map<String, String> payload = new HashMap<>();
+                    payload.put("name", "pippo"+i);
+                    payload.put("job", "pippo"+i);
+
+                    byte[] bytePayload = new ObjectMapper().writeValueAsBytes(payload);
+
+                    BasicHttpEntity httpEntity = new BasicHttpEntity();
+                    httpEntity.setContent(new ByteArrayInputStream(bytePayload));
+                    httpEntity.setContentLength(bytePayload.length);
+                    httpEntity.setContentType("application/json");
+
+
+                    HttpPut request = new HttpPut();
+                    request.setURI(URI.create(url));
+                    request.setEntity(httpEntity);
+
+                    ApacheHttpRequest<String> okHttpRequest = new ApacheHttpRequest<>(requestPrefix+i, request, dataPrefix+i);
+
+                    publisherChannel.send(okHttpRequest);
+
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        };
+
+        LinkedTransferQueue<ApacheHttpResponse<String>> responseQueue = resultQueue();
+
+        long startTime = System.currentTimeMillis();
+
+        IFlow currentFlow = QuasarFlow.newFlow()
+                .broadcastEmitter(requestTask)
+                .map(emitter -> new ApacheHttpProcessor<>(emitter)
+                        .processWithFanIn(fibers))
+                .addConsumer()
+                .consume(responseQueue::add)
+                .start();
+
+
+        List<ApacheHttpResponse<String>> results = this.getResults(responseQueue, requests, 3, TimeUnit.SECONDS);
+
+        System.out.println("Executed "+requests+" requests in parallel ("+fibers+" fibers) in: "+(System.currentTimeMillis()-startTime)+" ms");
+        System.out.println("Requests summed execution time: "+results.stream().mapToLong(ApacheHttpResponse::getExecution).sum()+" ms");
+
+        assertEquals(results.size(), requests);
+
+        for(ApacheHttpResponse<String> resp : results){
+            String idx0 = resp.getAttachedDatas().split(dataPrefix)[1];
+            String idx1 = resp.getRequestId().split(requestPrefix)[1];
+            assertEquals(idx0, idx1);
+        }
+
+        assertEquals(results.size(), requests);
+
+        //currentFlow.destroy();
+
+    }
+
 }
