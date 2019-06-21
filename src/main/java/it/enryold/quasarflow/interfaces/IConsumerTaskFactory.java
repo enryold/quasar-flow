e40a1a503b3516601cebe0ae73d2d12b616675a6
Enrico Vecchio <enrico.vecchio1@gmail.com>
2019-06-21 12:39:56 +0200

    Add suspendables + FiberChain

--- src/main/java/it/enryold/quasarflow/interfaces/IConsumerTaskFactory.java
+++ src/main/java/it/enryold/quasarflow/interfaces/IConsumerTaskFactory.java
@@ -1,9 +1,11 @@
 package it.enryold.quasarflow.interfaces;
 
 
+import co.paralleluniverse.fibers.Suspendable;
 
 @FunctionalInterface
 public interface IConsumerTaskFactory<I> {
 
+    @Suspendable
     IConsumerTask<I> build();
 }
--- src/main/java/it/enryold/quasarflow/interfaces/IRoutingKeyExtractor.java
+++ src/main/java/it/enryold/quasarflow/interfaces/IRoutingKeyExtractor.java
@@ -1,6 +1,7 @@
 package it.enryold.quasarflow.interfaces;
 
 
+import co.paralleluniverse.fibers.Suspendable;
 import it.enryold.quasarflow.models.utils.QRoutingKey;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 @FunctionalInterface
 public interface IRoutingKeyExtractor<Object> {
 
+    @Suspendable
     QRoutingKey extactRoutingKeyFromObject(Object o);
 }
